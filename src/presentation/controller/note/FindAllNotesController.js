39fb70dd2bc12b12f3e7e24cbeecafcf82d1b452
Onildo de Lima Goncalves <onildoscript@gmail.com>
2022-04-01 22:03:09 -0300

    Query search note per number nf

--- src/presentation/controller/note/FindAllNotesController.js
+++ src/presentation/controller/note/FindAllNotesController.js
@@ -1,12 +1,19 @@
 import { FindAllNotesUseCase } from "../../../application/use-cases/note/FindAllNotesUseCase.js"
+import { FindByNfNotesUseCase } from "../../../application/use-cases/note/FindByNfNotesUseCase.js"
 import { FindByIdUserUseCase } from "../../../application/use-cases/user/FindByIdUserUseCase.js"
 
 export async function FindAllNotesController(request, response) {
+  const { content } = request.query
   const { user_id } = request
   const { store } = await FindByIdUserUseCase(user_id)
 
-  const { page = 1 } = request.query
+  if (content) {
+    const notes = await FindByNfNotesUseCase(content, store)
 
+    return response.status(200).json({ notes })
+  }
+
+  const { page = 1 } = request.query
   const { notes, count } = await FindAllNotesUseCase(page, store)
 
   return response.status(200).json({ notes, ...count })
